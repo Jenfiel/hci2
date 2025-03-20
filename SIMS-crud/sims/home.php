e1aa3d8b06e51a99b509c2761b281cf535317dc7
Jenfiel <j.jejune18@gmail.com>
2025-03-20 21:23:27 +0800

    Add files via upload

---
+++ SIMS-crud/sims/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../sims/css/home.css">
+    <title>Student Management System</title>
+</head>
+<body>
+    <header> 
+        <nav>
+            <ul>
+                <li class="icon"><i class="fas fa-book"></i></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Subjects</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="signup.php" class="button">Sign In</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="container">
+        <div class="left">
+            <h1>Student Management System</h1>
+            <h3>Effortless Student Management</h3>
+            <p>Monitor student progress, manage courses, and enhance productivity with ease.</p>
+            <a href="signup.php" class="button">Learn More</a>
+        </div>
+        <div class="right">
+            <img src="../sims/img/Business Management.jpg" alt="Student Management">
+        </div>
+    </section>
+    <footer>
+        <p>&copy; 2025 Student Management System. All rights reserved. Empowering education, one student at a time.</p>
+    </footer>
+</body>
+</html>
